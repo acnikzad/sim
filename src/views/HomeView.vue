0734dd578c3fa49f0affd01f7c86ae1c6a95c8e7
Aria Nikzad <a.c.nikzad@gmail.com>
2022-03-04 15:56:38 -0800

    added stocks account

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,18 +5,27 @@
       <form>
         <input id="userIncome" type="text" placeholder="Enter Annual Income" v-model="income"/>
         <br>
-        <input id="userIncome" type="text" placeholder="Enter Percentage Saved" v-model="save_rate"/>
+        <input id="userIncome" type="text" placeholder="Percentage Saved" v-model="save_rate"/>
         <br>
-        <input id="userIncome" type="text" placeholder="Enter Monthly Bills:" v-model="monthly_bills"/>
+        <input id="userIncome" type="text" placeholder="Percentage Invested in Stocks" v-model="stocks_rate"/>
+        <br>
+        <input id="userIncome" type="text" placeholder="Enter Monthly Bills" v-model="monthly_bills"/>
         <br>
         <button type="button" v-on:click="returnStart()">Submit</button>
+
+        <button type="button" v-on:click="speedUp()">Speed Up</button>
+        <button type="button" v-on:click="speedDown()">Speed Down</button>
+
       </form>
       <h1>The Month is <span id="month"></span></h1>
       <h1>The Year is <span id="year"></span></h1>
       <h1>You are <span id="age"></span> years old</h1>
       <h2>Your income is ${{income}} per year</h2>
+
       <h2>You are saving {{save_rate}} of your income or {{save_amount}} per month</h2>
-      <h2>Your savings account is $<span id="savings"></span></h2>
+      <h2>Checking Account: $<span id="checking"></span></h2>
+      <h2>Savings Account: $<span id="savings"></span></h2>
+      <h2>Stocks Account: $<span id="stocks"></span></h2>
 
   </div>
 </template>
@@ -34,15 +43,24 @@ var formatter = new Intl.NumberFormat('en-US', {
 });
 
 var income = 0;
-var monthly_income = (income/12)
+var monthly_income = (income/12);
+var col_bills = 0;
 
 var save_rate = "";
 var save_amount = (monthly_income*save_rate);
 var save_account = 0;
 var save_rate = "";
 
+var stocks_rate = "";
+var stocks_amount = (monthly_income*stocks_rate);
+var stocks_account = 0;
+var stocks_rate = "";
+var stock_market_rate = .1
+var check_account = null;
+
 var income_tax = 0.08;
 
+var speed = 350;
 var count = -1;
 var rate = .4;
 var product = 0;
@@ -61,6 +79,9 @@ export default {
       income:null,
       income_tax:null,
       income_tax: .08,
+      check_account: 500,
+      speed: 150,
+
       // save_rate:null,
       // monthly_income:null,
       // save_amount:null,
@@ -70,13 +91,15 @@ export default {
 
   created: function() {
 
-      var intId = setInterval(counter, 750);
+      var intId = setInterval(counter, speed);
 
       function counter() {
         document.getElementById("month").innerHTML = month;
         document.getElementById("year").innerHTML = year;
         document.getElementById("age").innerHTML = age;
         document.getElementById("savings").innerHTML = save_account;
+        document.getElementById("checking").innerHTML = check_account;
+        document.getElementById("stocks").innerHTML = stocks_account;
         console.log(++count);
         oneMonth()
         // console.log("*****", save_account, "*****");
@@ -101,12 +124,17 @@ export default {
         console.log("this is one year");
         year = year + 1
         age = age + 1
+        stocks_account = stocks_account + (stocks_account*stock_market_rate)
+        console.log("/////////////stock_market_rate is:", stock_market_rate)
       };
 
       function oneMonth() {
         console.log("this is one month");
-        save_account = save_account + save_amount;
         month = (months[count])
+        save_account = save_account + save_amount;
+        stocks_account = stocks_account + stocks_amount;
+        check_account = check_account + (monthly_income - save_amount);
+
       }
 
       // function save_account() {
@@ -119,6 +147,16 @@ export default {
     methods: {
       startHere() {
         console.log("x");
+      },
+
+      speedUp() {
+        speed = (speed - 100);
+        console.log("THE SPEED IS: ", speed);
+      },
+
+      speedDown() {
+        speed = (speed + 100);
+        console.log("THE SPEED IS: ", speed);
       },
 
       returnStart() {
@@ -141,9 +179,9 @@ export default {
         // } else {
         //   income_tax = .113
         // }
-
         save_amount = (monthly_income*this.save_rate),
-        save_account = save_account + save_amount
+        // save_account = save_account + save_amount,
+        stocks_amount = (monthly_income*this.stocks_rate),
         // formatter.format(monthly_income);
         console.log(new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(monthly_income));
         console.log(new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(save_account));
